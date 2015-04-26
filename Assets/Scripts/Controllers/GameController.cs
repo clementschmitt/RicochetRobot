fc95856717e1dbbe10e2ed340260656936de0286
kbollini <kevinbollini@gmail.com>
2015-04-26 14:36:41 +0200

    Basic unity project, with few elements

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private GameController _instance;
+
+    public GameController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Controller isn't instaciated yet");
+                return null;
+            }
+
+            return _instance;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	    _instance = this;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
